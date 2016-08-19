8567b4a946509c0ad2fcbb3257cd4b892a938a41
CTRLSOFT-9BZ5JW\Роман <romaaverkin@gmail.com>
2016-08-19 07:43:22 +0700

    Настроил ЧПУ ссылки

--- config/web.php
+++ config/web.php
@@ -10,6 +10,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'rTiJQSnc0GwK_o1N8BSsgkhzkRJVfkn6',
+            'baseUrl' => '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -38,14 +39,12 @@ $config = [
             ],
         ],
         'db' => require(__DIR__ . '/db.php'),
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
